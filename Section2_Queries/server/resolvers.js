5783a6b1e0e8aeeae8f4da59fb579d7fad55ec97
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-06-30 02:50:20 -0500

    Finished section 2, can now view jobs, companies, and jobs at specific companies

---
+++ Section2_Queries/server/resolvers.js
@@ -0,0 +1,20 @@
+const db = require('./db');
+
+const Query = {
+    company: (root, {id}) => db.companies.get(id),
+    job: (root, {id}) => db.jobs.get(id),
+    jobs: () => db.jobs.list()
+};
+
+// one to many relationship company to jobs
+
+const Company = {
+    jobs: (company) => db.jobs.list()
+        .filter((job) => job.companyId === company.id)
+};
+
+const Job = {
+    company: (job) => db.companies.get(job.companyId) 
+};
+
+module.exports = { Query, Job, Company };
